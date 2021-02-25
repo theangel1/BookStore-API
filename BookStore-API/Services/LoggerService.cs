22bd95a02cca969e5f53a8777d9be19a89120601
Angel Pinilla Ibarra <angel.pinilla@outlook.es>
2021-02-25 17:01:49 -0300

    Add project files.

---
+++ BookStore-API/Services/LoggerService.cs
@@ -0,0 +1,34 @@
+﻿using BookStore_API.Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);            
+        }
+
+        public void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
---
+++ BookStore-API/Startup.cs
@@ -0,0 +1,97 @@
+using BookStore_API.Contracts;
+using BookStore_API.Data;
+using BookStore_API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BookStore_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddCors(o => {
+                o.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
+                {
+                    Title = "Book Store API", 
+                    Version = "v1", 
+                    Description = "This is an educational API for a book store"
+                });
+
+                var xfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
+                c.IncludeXmlComments(xpath);
+            });
+
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+
+            services.AddControllers();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");                
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store API");
+                c.RoutePrefix = "";
+            });
+
+            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
